c92e843483c3445dbf545042210836868a504b35
huchoi <nyz1058@nate.com>
2021-09-01 18:41:41 +0900

    chage sclinet class struct

--- includes/client.hpp
+++ includes/client.hpp
@@ -4,32 +4,44 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cstdio>
 #include <exception>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <map>
+#include <unistd.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #include "msg_checker.hpp"
-#include "utils_for_parse.hpp"
 
 class client
 {
 	public:
-		typedef typename msg_checker::return_type checker_return_type;
-		
+		typedef msg_checker::return_type checker_return_type;
+
 	private:
-		//리퀘스트 메시지 파싱해서 _parsed_request_msg변수에 저장
-		std::string first_line;
-		std::map<std::string, std::string> header_field;
+		int socker_num;
+		//리퀘스트 메시지 파싱
+		void parse_msg(std::string &request_msg);
+		//리퀘스트 메시지 파싱해서 아래의 두 변수에 저장
+		std::string _first_line;
+		std::map<std::string, std::string> _header_field;
 		//msg_checker 객체
-		msg_checker obj;
+		msg_checker _obj;
 		//msg_chekcer 객체가 점검을 끝내고 반환하는 값을 info에 저장
-		checker_return_type info;
+		checker_return_type _info;
+		//parse_utils
+		std::string get_next_line(const std::string &src);
+		std::string ft_strtok(std::string &src, std::string deli);
+		bool ft_contain(const std::string &src, std::string sub);
 
 	public:
-		client(std::string request_msg);
+		//client(std::string request_msg);
+		client(int socket);
 		std::string get_response();
 };
 
--- includes/utils_for_parse.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <string>
-
-std::string get_next_line(const std::string &src);
-std::string ft_strtok(std::string &src, std::string deli);
-bool ft_contain(const std::string &src, std::string sub);
