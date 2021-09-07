a73514cf82360bcfd6686db0948d886e2e208630
BroJoon <ydngjink1234@gmail.com>
2021-09-07 14:48:15 +0900

    add: listen도 파싱해서 set자료구조에 넣었습니다. 소켓 만들떄 이걸로 만드시면 될것 같아요

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -9,13 +9,13 @@
 
 namespace ft
 {
+	void config_print();
 	void split(const std::string &src, const std::string& delimiter, std::vector<std::string> &dst);
 	bool isMethods(const std::string &src);
 	bool isknown(const std::string &src);
 	std::string get_next_line(const std::string &src);
 	std::string ft_strtok(std::string &src, std::string deli);
 	bool ft_contain(const std::string &src, std::string sub);
-	void config_print();
 	const std::string currentDateTime();
 	std::string getFileModifiedTime(const std::string &src) ;
 }
